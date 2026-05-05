b6f2f9fc868be359022fc6e8258bead397a3e00b
kobadaidesu <daigokobayashi04@gmail.com>
2026-05-06 06:31:23 +0900

    J

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kobadai <kobadai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/06 00:00:00 by dakobaya          #+#    #+#             */
-/*   Updated: 2026/05/06 03:30:46 by kobadai          ###   ########.fr       */
+/*   Created: 2026/05/03 00:01:33 by kobadai           #+#    #+#             */
+/*   Updated: 2026/05/06 06:26:30 by kobadai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,23 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*new_node;
-	void	*content;
+	t_list	*first;
+	t_list	*new;
 
-	if (!lst || !f || !del)
+	if (!f || !del)
 		return (NULL);
-	new_list = NULL;
+	first = NULL;
 	while (lst)
 	{
-		content = f(lst->content);
-		new_node = ft_lstnew(content);
-		if (!new_node)
+		new = ft_lstnew((*f)(lst->content));
+		if (!new)
 		{
-			del(content);
-			ft_lstclear(&new_list, del);
+			ft_lstclear(&first, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, new_node);
+		ft_lstadd_back(&first, new);
 		lst = lst->next;
 	}
-	return (new_list);
+	return (first);
+}
 }
