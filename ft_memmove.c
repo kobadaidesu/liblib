1a4fd9e76639067ff06bdc733906939c92b003a0
kobadaidesu <daigokobayashi04@gmail.com>
2026-05-07 15:09:27 +0900

    j

--- ft_memmove.c
+++ ft_memmove.c
@@ -17,7 +17,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
-	if (!dest && !src)
+	if (!dest || !src)
 		return (dest);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
